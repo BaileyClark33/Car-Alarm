6fab2cbccf1d95214bd8b629f0797f8fcde77931
BaileyClark33 <91906664+BaileyClark33@users.noreply.github.com>
2025-01-22 14:54:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,119 @@
 #include "arm_book_lib.h"
 #include "mbed.h"
+#define daylightMin 100
+#define duskMax 50
+
+/* 
+Ignition Subsystem
+When the driver sits down, display the message, 
+“Welcome to enhanced alarm system model 218-W25”.
+Indicate ignition enabled with the green LED only when 
+both seats are occupied and both seatbelts are fastened.
+ When the ignition button is pushed:
+-        If the ignition enabled (green LED) is lit, then normal ignition occurs. 
+Light the blue LED and extinguish the green LED. Display the message, “Engine started.”
+-        If the ignition is not enabled (green LED not lit), then ignition is inhibited. 
+Sound the alarm buzzer; display the message, “Ignition inhibited” and display 
+all the reasons why the ignition was inhibited: “Passenger seat not occupied,” 
+“Driver seatbelt not fastened,” etc.
+Once the error messages are displayed, the system allows additional start attempts. 
+(note this change from Project 1 requirements)
+Keep the engine running even if the driver or passenger should remove their seat 
+belts or exit the vehicle.
+When the engine is running, stop the engine when the ignition button is pushed 
+and then released. (note this change from Project 1 requirements)
 
 
-/* According to a provision of the New York State Department of Motor Vehicles:
-“If you have a junior learner permit, you may drive in upstate New York between
-the hours of 5 AM and 9 PM only under the direct supervision of a person who is
-at least age 21 and has a license valid for the vehicle you are driving.” Some
-New York public high schools are looking to upgrade the car alarm software in
-their Driver’s Education vehicles to help enforce this provision and also to
-enhance driver and passenger safety.
+Headlight subsystem
+If the engine is running, and the user selects: ON, turn on both low beam lamps; 
+if they select OFF, turn off all lamps.
+If the user selects the AUTO position: 
+When the light level is above a “daylight” level, switch headlights off after 
+a delay of 2 seconds.
+When the light level is below a “dusk” level, switch headlights on after a delay of 1 second.
+When between these two levels, the headlights should hold their previous state. 
+Turn off all headlights when the engine is off. 
 */
+
+void greenUpdate() {
+  // Light the green LED only when both seats are occupied and both
+  // seatbelts are fastened.
+  if (driveSeatUsed == ON && passSeatUsed == ON && driveBelt == ON &&
+        passBelt == ON) {
+      green = ON;
+    } else {
+      green = OFF;
+    }
+}
+
+void ignitionRun() {  
+    // -        If the green LED is lit, then normal ignition occurs.
+    //          Light the blue LED and extinguish the green LED. Display
+    //          the message, “Engine started.”
+    blue = ON;
+    green = OFF;
+    system.write("Engine Started\n", 16);
+  
+  while (ignition == false) {
+    // If the engine is running, and the user selects: ON, turn on both low beam lamps;
+if (headlightSelect == ON) {
+  rightLamp == ON
+  leftLamp == ON 
+} else if (headlightSelect == OFF) {  // if they select OFF, turn off all lamps.
+  rightLamp == OFF
+  leftLamp == OFF
+} else if (headlightSelect == AUTO) {  // If the user selects the AUTO position: 
+  if (lightSensor >= daylightMin) { // When the light level is above a “daylight” level, 
+    // switch headlights off after a delay of 2 seconds.
+    delay(2000);
+      rightLamp == OFF;
+      leftLamp == OFF;
+  } else if (lightSensor <= duskMax) {  // When the light level is below a “dusk” level, 
+    // switch headlights on after a delay of 1 second.
+    delay(1000;
+    rightLamp == ON
+    leftLamp == ON 
+// When between these two levels, the headlights should hold their previous state.
+  }
+}
+  }
+  // Keep the engine running even if the driver or passenger should remove their seat 
+// belts or exit the vehicle.
+  while (ignition == true) {
+  }
+// When the engine is running, stop the engine when the ignition button is pushed 
+// and then released. (note this change from Project 1 requirements)
+  // Turn off all headlights when the engine is off.
+  rightLamp == OFF
+  leftLamp == OFF
+    blue = OFF;
+    green = OFF;
+    system.write("Engine Stopped\n", 16);
+}
+  
+void ignitionFail() {
+  // -        If the green LED is not lit, then ignition is inhibited.
+    // Sound the alarm buzzer;
+    alarm.output();
+    //          display the message, “Ignition inhibited” and
+    system.write("Ignition inhibited\n", 20);
+    //     display all
+    //          the reasons why the ignition was inhibited: “Passenger
+    //          seat not occupied,” “Driver seatbelt not fastened,” etc.
+    if (driveSeatUsed == OFF) {
+      system.write("Driver seat unoccupied\n", 24);
+    }
+    if (passSeatUsed == OFF) {
+      system.write("Passenger seat unoccupied\n", 27);
+    }
+    if (driveBelt == OFF) {
+      system.write("Driver seatbelt not fastened\n", 30);
+    }
+    if (passBelt == OFF) {
+      system.write("Passenger seatbelt not fastened\n", 31);
+    }
+}
+
 int main() {
   // Driver seat occupancy sensor (provides contact closure when occupied);
   // Passenger seat occupancy sensor (provides contact closure when occupied);
@@ -21,94 +125,52 @@ int main() {
   DigitalIn driveBelt(D6);
   DigitalIn passBelt(D7);
   DigitalIn ignition(D5);
-
-
   // Alarm buzzer (sounds when ignition is inhibited);
   // Green indicator LED (lights when ignition is enabled);
   //  Blue indicator LED (lights when the engine has started);
   DigitalInOut alarm(PE_10);
   DigitalOut green(LED1);
   DigitalOut blue(LED2);
-
+  // Headlight mode selector (provides ON, OFF, or AUTO signals);
+  // Light sensor (provides ambient light level signal);
+  // Left low beam lamp, right low beam lamp
+  DigitalIn headlightSelect();
+  DigitalIn lightSensor();
+  DigitalIn leftLamp();
+  DigitalIn rightLamp();
 
   driveSeatUsed.mode(PullDown);
   passSeatUsed.mode(PullDown);
   driveBelt.mode(PullDown);
   passBelt.mode(PullDown);
   ignition.mode(PullDown);
-
-
+  
   UnbufferedSerial system(USBTX, USBRX, 115200);
 
-
-  //  When the driver sits down, display the message, “Welcome to enhanced
-  //  alarm system model 218-W24”.
-  while (driveSeatUsed == OFF) {
-  }
-
-
+// Once the error messages are displayed, the system allows additional start attempts. 
+// (note this change from Project 1 requirements)
+while (true) {
+//   When the driver sits down, display the message, 
+// “Welcome to enhanced alarm system model 218-W25”.
+while (driveSeatUsed == OFF) {
+}
   system.write("Welcome to enhanced alarm system model 218-W24\n", 47);
-  // Light the green LED only when both seats are occupied and both
-  // seatbelts are fastened.
-  // system.write("test\n", 6);
-
-
-  while (ignition == OFF) {
-    if (driveSeatUsed == ON && passSeatUsed == ON && driveBelt == ON &&
-        passBelt == ON) {
-      green = ON;
-    } else {
-      green = OFF;
-    }
-  }
-  //  When the ignition button is pushed:
-
-
-  // The user only gets one try to press the ignition button, and if the alarm
-  // sounds, or the car starts, then the system must be reset.
-
-
-  if (green == ON) {
-    // -        If the green LED is lit, then normal ignition occurs.
-    //          Light the blue LED and extinguish the green LED. Display
-    //          the message, “Engine started.”
-    blue = ON;
-    green = OFF;
-    system.write("Engine Started\n", 16);
-
-
-   
+// Indicate ignition enabled with the green LED only when 
+// both seats are occupied and both seatbelts are fastened
+while (ignition == OFF) {
+    greenUpdate()
+}
+//  When the ignition button is pushed:
+// -        If the ignition enabled (green LED) is lit, then normal ignition occurs. 
+// Light the blue LED and extinguish the green LED. Display the message, “Engine started.”
+if (green == ON) {
+    ignitionRun()
+// -        If the ignition is not enabled (green LED not lit), then ignition is inhibited. 
+// Sound the alarm buzzer; display the message, “Ignition inhibited” and display 
+// all the reasons why the ignition was inhibited: “Passenger seat not occupied,” 
+// “Driver seatbelt not fastened,” etc.
   } else {
-      // -        If the green LED is not lit, then ignition is inhibited.
-    // Sound the alarm buzzer;
-    alarm.output();
-    //          display the message, “Ignition inhibited” and
-
-
-    //     display all
-    //          the reasons why the ignition was inhibited: “Passenger
-    //          seat not occupied,” “Driver seatbelt not fastened,” etc.
-    system.write("Ignition inhibited\n", 20);
-
-
-    if (driveSeatUsed == OFF) {
-      system.write("Driver seat unoccupied\n", 24);
-    }
-
-
-    if (passSeatUsed == OFF) {
-      system.write("Passenger seat unoccupied\n", 27);
-    }
-
-
-    if (driveBelt == OFF) {
-      system.write("Driver seatbelt not fastened\n", 30);
-    }
-
-
-    if (passBelt == OFF) {
-      system.write("Passenger seatbelt not fastened\n", 31);
-    }
+    ignitionFail()
   }
 }
-
+}
